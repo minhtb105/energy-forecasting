d744e48294bada13a0f9e87054cadd3f710cdd53
minhtb105 <tranbinhminh_t64@hus.edu.vn>
2025-08-24 22:03:43 +0700

    change future deprecated parameter artifact_path to name

--- src/mlflow_utils.py
+++ src/mlflow_utils.py
@@ -5,7 +5,7 @@ def setup_mlflow(experiment_name="gefcom2012_baseline", tracking_uri="sqlite:///
     mlflow.set_tracking_uri(tracking_uri)
     mlflow.set_experiment(experiment_name)
 
-def log_model_results(model_name, model, metrics, params, artifact_path="model"):
+def log_model_results(model_name, model, metrics, params, name="model"):
     with mlflow.start_run(run_name=model_name):
         for k, v in metrics.items():
             mlflow.log_metric(k, v)
