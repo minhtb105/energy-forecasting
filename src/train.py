4859c325b304b329d9ae85e47c25fdd353419155
minhtb105 <tranbinhminh_t64@hus.edu.vn>
2025-08-24 21:35:33 +0700

    feat: add MLflow experiment tracking with learning curve across sample size

--- src/train.py
+++ src/train.py
@@ -1,7 +1,7 @@
 import pandas as pd
 from sklearn.model_selection import train_test_split
 from data_prep import load_data, encode_season
-from utils import add_calendar_features
+from utils import add_cyclical_time_features, add_calendar_features
 from models import run_lazy_regressor
 from evaluation import evaluate
 from mlflow_utils import setup_mlflow, log_model_results
@@ -15,12 +15,11 @@ train_df, test_df = load_data(
     "data/global_energy_forcasting_2012/processed/test.csv"
 )
 
-train_df.drop(["Unnamed: 0"], axis=1, inplace=True)
-test_df.drop(["Unnamed: 0"], axis=1, inplace=True)
-
 # feature engineering
 train_df = add_calendar_features(train_df)
 test_df = add_calendar_features(test_df)
+train_df = add_cyclical_time_features(train_df)
+test_df = add_cyclical_time_features(test_df)
 
 train_df, test_df = encode_season(train_df, test_df)
 
@@ -31,12 +30,24 @@ X_tr, X_val, y_tr, y_val = train_test_split(
     X_train, y_train, test_size=0.2, shuffle=False
 )
 
-# run LazyRegressor
-models, predictions, model_dict = run_lazy_regressor(X_tr, X_val, y_tr, y_val)
+sample_sizes = [5000, 10000, 20000, 40000]
 
-# log results
-for model_name, model in model_dict.items():
-    y_pred_val = model.predict(X_val)
-    metrics = evaluate(y_val, y_pred_val)
-    params = {"num_train_samples": len(X_tr), "num_val_samples": len(X_val)}
-    log_model_results(model_name, model, metrics, params)
+for n in sample_sizes:
+    # Lấy sample từ train/val
+    X_tr_sample = X_tr.sample(n=n, random_state=42)
+    y_tr_sample = y_tr.loc[X_tr_sample.index]
+
+    X_val_sample = X_val.sample(n=2000, random_state=42)
+    y_val_sample = y_val.loc[X_val_sample.index]
+
+    # run LazyRegressor
+    models, predictions, model_dict = run_lazy_regressor(X_tr_sample, 
+                                                        X_val_sample, 
+                                                        y_tr_sample, y_val_sample)
+
+    # log results
+    for model_name, model in model_dict.items():
+        y_pred_val = model.predict(X_val_sample)
+        metrics = evaluate(y_val_sample, y_pred_val)
+        params = {"num_train_samples": len(X_tr_sample), "num_val_samples": len(X_val_sample)}
+        log_model_results(model_name, model, metrics, params)
