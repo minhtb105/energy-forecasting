eabb569fa65569449b2ce85f68f1e18ac62ca312
minhtb105 <tranbinhminh_t64@hus.edu.vn>
2025-08-24 22:01:38 +0700

    remove unexpected keyword argument 'squared' in mean squared error

--- src/evaluation.py
+++ src/evaluation.py
@@ -1,7 +1,7 @@
 from sklearn.metrics import mean_squared_error, mean_absolute_error, r2_score
 
 def evaluate(y_true, y_pred):
-    rmse = mean_squared_error(y_true, y_pred, squared=False)
+    rmse = mean_squared_error(y_true, y_pred)
     mae = mean_absolute_error(y_true, y_pred)
     r2 = r2_score(y_true, y_pred)
     
