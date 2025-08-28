d847cde0796d22b80666ae80f2e56cf457e564fa
minhtb105 <tranbinhminh_t64@hus.edu.vn>
2025-08-28 09:52:17 +0700

    add artifact_path [arameter to mlflow sklearn log_model

--- src/mlflow_utils.py
+++ src/mlflow_utils.py
@@ -5,12 +5,16 @@ def setup_mlflow(experiment_name="gefcom2012_baseline", tracking_uri="sqlite:///
     mlflow.set_tracking_uri(tracking_uri)
     mlflow.set_experiment(experiment_name)
 
-def log_model_results(model_name, model, metrics, params, name="model"):
+def log_model_results(model_name, model, metrics, params, name="model", input_example=None):
     with mlflow.start_run(run_name=model_name):
         for k, v in metrics.items():
             mlflow.log_metric(k, v)
             
         for k, v in params.items():
             mlflow.log_param(k, v)
-            
-        mlflow.sklearn.log_model(model, artifact_path=artifact_path)
+        
+        mlflow.sklearn.log_model(
+            model,
+            artifact_path=name,
+            input_example=input_example
+        )
